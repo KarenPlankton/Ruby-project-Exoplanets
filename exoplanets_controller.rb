c08d6a58c1b1a970c3ddbadd841152628d0f6d06
Anna <elbereth_spencer@hotmail.com>
2020-03-23 06:43:42 +0000

    adding explicit controller inheritance

--- exoplanets_controller.rb
+++ exoplanets_controller.rb
@@ -4,80 +4,82 @@ require( 'pry-byebug' )
 
 require_relative( './models/exoplanet.rb' )
 also_reload( './models/*' )
+class Exoplanet_Controller < Sinatra::Application
+  get '/' do
 
-get '/' do
+    erb( :index )
+  end
 
-  erb( :index )
-end
+  get '/search' do
 
-get '/search' do
+    erb( :search )
+  end
 
-  erb( :search )
-end
+  get '/documentation' do
+    erb( :documentation)
+  end
 
-get '/documentation' do
-  erb( :documentation)
-end
-
- post '/result_presentation' do
-   user_input=params[:find_operation]
-   puts user_input
-   if user_input == "host_stars"
-     @planets = Exoplanet.host_stars
+  post '/result_presentation' do
+    user_input=params[:find_operation]
+    puts user_input
+    if user_input == "host_stars"
+      @planets = Exoplanet.host_stars
     elsif user_input == "habitable_exoplanets"
-    @planets = Exoplanet.habitable_exoplanets
-    
-  elsif user_input=="earth_size_planets"
-    @planets=Exoplanet.find_earth_size_planets
-  elsif user_input=="superearth_size_planets"
-    @planets=Exoplanet.find_super_earth_planets
-  elsif user_input=="mini_neptune_size_planets"
-    @planets=Exoplanet.find_mini_neptune_planets
-  elsif user_input=="neptune_size_planets"
-    @planets=Exoplanet.find_neptune_size_planets
-  elsif user_input=="jupiter_size_planets"
-    @planets=Exoplanet.find_jupiter_size_planets
-  elsif user_input=="super_giant_size_planets"
-    @planets=Exoplanet.find_super_giants_planets
-  elsif user_input=="planets_in_habitable_zone"
-    @planets=Exoplanet.find_planets_in_habitable_zone
-  elsif user_input=="retrieve_all_planets"
-    @planets=Exoplanet.all
+      @planets = Exoplanet.habitable_exoplanets
 
-      end
+    elsif user_input=="earth_size_planets"
+      @planets=Exoplanet.find_earth_size_planets
+    elsif user_input=="superearth_size_planets"
+      @planets=Exoplanet.find_super_earth_planets
+    elsif user_input=="mini_neptune_size_planets"
+      @planets=Exoplanet.find_mini_neptune_planets
+    elsif user_input=="neptune_size_planets"
+      @planets=Exoplanet.find_neptune_size_planets
+    elsif user_input=="jupiter_size_planets"
+      @planets=Exoplanet.find_jupiter_size_planets
+    elsif user_input=="super_giant_size_planets"
+      @planets=Exoplanet.find_super_giants_planets
+    elsif user_input=="planets_in_habitable_zone"
+      @planets=Exoplanet.find_planets_in_habitable_zone
+    elsif user_input=="retrieve_all_planets"
+      @planets=Exoplanet.all
 
-   erb( :result_presentation )
- end
+    end
 
- get '/result_details/:id' do
-   @planet =Exoplanet.find(params[:id].to_i)
-   erb(:result_details)
- end
+    erb( :result_presentation )
+  end
 
- get '/delete/:id' do
-   @planet =Exoplanet.find(params[:id].to_i)
-   # binding.pry
-   @planet.delete
-   erb(:delete)
- end
+  get '/result_details/:id' do
+    @planet =Exoplanet.find(params[:id].to_i)
+    erb(:result_details)
+  end
 
- get '/insert' do
-   erb(:insert)
- end
+  get '/delete/:id' do
+    @planet =Exoplanet.find(params[:id].to_i)
+    # binding.pry
+    @planet.delete
+    erb(:delete)
+  end
 
- post '/insert_action' do
-   @planet =Exoplanet.new(params)
-   @planet.save
-   erb(:insert_action)
- end
+  get '/insert' do
+    erb(:insert)
+  end
 
- get '/update/:id' do
-   @planet =Exoplanet.find(params[:id].to_i)
-   erb(:update)
- end
+  post '/insert_action' do
+    @planet =Exoplanet.new(params)
+    @planet.save
+    erb(:insert_action)
+  end
 
- post '/update_action' do
-   @planet =Exoplanet.new(params)
-   @planet.update
-   erb(:update_action)
- end
+  get '/update/:id' do
+    @planet =Exoplanet.find(params[:id].to_i)
+    erb(:update)
+  end
+
+  post '/update_action' do
+    @planet =Exoplanet.new(params)
+    @planet.update
+    erb(:update_action)
+  end
+end
+Exoplanet_Controller.run!
